ce85161dc12e36f01cae0bb93a8404fd62e18823
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-09-26 16:32:14 -0300

    mostrar el detalle de una pelicula

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -2,9 +2,20 @@ const db = require("../database/models");
 
 const moviesController = {
     list: (req, res) => {
-        db.Movie.findAll().then((movies) => {
-            return res.render("moviesList", { movies });
-        });
+        db.Movie.findAll()
+            .then((movies) => {
+                return res.render("moviesList", { movies });
+            })
+            .catch((err) => console.log(err));
+    },
+    detail: (req, res) => {
+        const { id } = req.params;
+
+        db.Movie.findByPk(id)
+            .then((movie) => {
+                return res.render("moviesDetail", { movie });
+            })
+            .catch((err) => console.log(err));
     },
 };
 
