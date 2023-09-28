9cb10bd5f372ed5fa0c7abba8b4228b3eee4e2e9
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-09-28 17:13:07 -0300

    agregar paquete method-override

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const methodOverride = require("method-override");
 
 const indexRouter = require("./routes/index");
 
@@ -16,6 +17,9 @@ app.use(express.static(path.resolve(__dirname, "../public")));
 
 //URL encode  - Para que nos pueda llegar la información desde el formulario al req.body
 app.use(express.urlencoded({ extended: false }));
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
 
 app.use("/", indexRouter);
 app.use(moviesRoutes);
